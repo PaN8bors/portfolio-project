3835769eb60749614860de5c4c421cee81817ec0
Parish Neighbors <pn8bors@gmail.com>
2022-03-28 11:07:26 -0500

    Fifth commit

--- src/home/Menu.js
+++ src/home/Menu.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import "./Menu.css";
+import Pdf from "../Images/032822.pdf";
 
 export default function Menu() {
     return (
@@ -8,7 +9,7 @@ export default function Menu() {
             <Link to="/profile"><h1 className="head2">-Profile</h1></Link>
             <Link to="/projects"><h1 className="head2">-Projects</h1></Link>
             <Link to="/projects2"><h1 className="head2">-Projects 2</h1></Link>
-            <h1 className="head2">-Resume</h1>
+            <a href ={Pdf}><h1 className="head2">-Resume</h1></a>
         </div>
         
     )
